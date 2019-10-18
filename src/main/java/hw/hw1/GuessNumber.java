18fc5bb671601dac3ca2d95cf6df46a9ecde1b38
Zem <karimovazemfira@yahoo.com>
2019-10-18 22:04:34 +0400

    --initial

---
+++ src/main/java/hw/hw1/GuessNumber.java
@@ -0,0 +1,39 @@
+package hw.hw1;
+
+import  java.util.Random;
+import  java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100);
+        Scanner input = new Scanner(System.in);
+        int guess;
+        boolean win = false;
+
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Let the game begin!");
+
+        System.out.println("Guess a number between 1 and 100: ");
+
+        while (!win) {
+
+            guess = input.nextInt();
+
+            if (guess == numberToGuess) {
+                win = true;
+            } else if (guess < numberToGuess) {
+                System.out.println("Your guess is too small. Please try again..");
+            } else
+                System.out.println("Your guess is too big. Please try again..");
+        }
+
+
+        System.out.println("Congratulations," + name + "!");
+    }
+
+}
+
