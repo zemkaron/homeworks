385e4ad220dd5f4ed28acb993bf66b99c3dbec56
Zem <karimovazemfira@yahoo.com>
2019-11-10 17:56:55 +0400

    --initial

--- src/main/java/hw/hw2/AreaShooting.java
+++ src/main/java/hw/hw2/AreaShooting.java
@@ -5,11 +5,6 @@ public class AreaShooting {
         System.out.println("All set. Get ready to rumble!");
         char [][] a = new char[6][6];
 
-        for (int i = 0; i < a.length-1 ; i++ ){
-                for (int j = 0; j < a.length-1; j++ ){
-                    System.out.print("-");
-                }
-            System.out.println();
-        }
+
     }
 }
